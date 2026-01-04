20b0b6fa4a58244c5d197b3e48676d945b8a34eb
Sovanna Ra <sovann.ra277@gmail.com>
2026-01-04 13:07:30 +0700

    setup app.vue, pages, and default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink>
+      |
+      <NuxtLink to="/posts">Posts</NuxtLink>
+    </header>
+
+    <slot />
+  </div>
+</template>

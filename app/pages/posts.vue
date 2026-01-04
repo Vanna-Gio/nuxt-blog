2465c731179f23c82f42f8df38624188aafa4702
Sovanna Ra <sovann.ra277@gmail.com>
2026-01-04 13:30:31 +0700

    add posts list, dynamic routes, fetch,and SEO

--- app/pages/posts.vue
+++ app/pages/posts.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <h1>Posts page</h1>
+    <h1>Posts</h1>
+
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="`/posts/${post.id}`">
+          {{ post.title }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const { data: posts } = await useFetch(
+  'https://jsonplaceholder.typicode.com/posts?_limit=5'
+)
+</script>
---
+++ app/pages/posts/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="post">
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useHead } from '#imports'
+
+useHead({
+  title: post.value?.title,
+  meta: [
+    {
+      name: 'description',
+      content: post.value?.body.slice(0, 100)
+    }
+  ]
+})
+</script>
+
